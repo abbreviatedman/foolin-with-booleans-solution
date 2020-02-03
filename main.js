95240f7af0fe8bb1903f64071e90f02f57fc8fc9
Colin Jaffe <balloonasaurus@gmail.com>
2020-02-03 10:09:15 -0500

    Initial commit from challenge repository.

---
+++ main.js
@@ -0,0 +1,78 @@
+/********************
+ * YOUR CODE BELOW! *
+ ********************/
+
+
+
+
+
+/********************************************************************************************
+ * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
+ *                              THIS IS FOR INTERNAL USE ONLY                               *
+ *                          DON'T ADD TO OR CHANGE ANYTHING BELOW!                          *
+ ********************************************************************************************/
+
+
+if (typeof iGetTheJoke === 'undefined') {
+  iGetTheJoke = undefined;
+}
+
+if (typeof havingFun === 'undefined') {
+  havingFun = undefined;
+}
+
+if (typeof learning === 'undefined') {
+  learning = undefined;
+}
+
+if (typeof killingIt === 'undefined') {
+  killingIt = undefined;
+}
+
+if (typeof isOpposite === 'undefined') {
+  isOpposite = undefined;
+}
+
+if (typeof returnFalse === 'undefined') {
+  returnFalse = undefined;
+}
+
+if (typeof both === 'undefined') {
+  both = undefined;
+}
+
+if (typeof either === 'undefined') {
+  either = undefined;
+}
+
+if (typeof firstOnly === 'undefined') {
+  firstOnly = undefined;
+}
+
+if (typeof secondOnly === 'undefined') {
+  secondOnly = undefined;
+}
+
+if (typeof neither === 'undefined') {
+  neither = undefined;
+}
+
+if (typeof itsComplicated === 'undefined') {
+  itsComplicated = undefined;
+}
+
+
+module.exports = {
+  iGetTheJoke,
+  havingFun,
+  learning,
+  killingIt,
+  returnFalse,
+  isOpposite,
+  both,
+  either,
+  firstOnly,
+  secondOnly,
+  neither,
+  itsComplicated,
+}
